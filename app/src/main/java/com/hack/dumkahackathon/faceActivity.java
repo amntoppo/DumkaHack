5ee9b154fe38c5bc93ff8bb08b610181ba4c46b6
amntoppo <amntoppo@gmail.com>
2019-04-07 12:55:00 +0530

    Added Aadhar Login

---
+++ app/src/main/java/com/hack/dumkahackathon/faceActivity.java
@@ -0,0 +1,13 @@
+package com.hack.dumkahackathon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class faceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
